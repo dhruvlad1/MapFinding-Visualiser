bb0ab88f893f623adaeb923bf7d19028da83638d
krish <krishsharma1505@gmail.com>
2026-04-17 11:13:51 +0530

    metrics

--- src/models/PathfindingState.js
+++ src/models/PathfindingState.js
@@ -5,82 +5,94 @@ import Greedy from "./algorithms/Greedy";
 import PathfindingAlgorithm from "./algorithms/PathfindingAlgorithm";
 
 export default class PathfindingState {
-    static #instance;
+    static _instance;
 
-    /**
-     * Singleton class
-     * @returns {PathfindingState}
-     */
     constructor() {
-        if (!PathfindingState.#instance) {
+        if (!PathfindingState._instance) {
             this.endNode = null;
             this.graph = null;
             this.finished = false;
             this.algorithm = new PathfindingAlgorithm();
-            PathfindingState.#instance = this;
+            this._startTime = 0;
+            this._nodesExplored = 0;
+            this.metrics = {
+                algorithmName: "",
+                execTime: 0,
+                nodesExplored: 0,
+                pathLength: 0,
+                memoryUsage: 0,
+            };
+            PathfindingState._instance = this;
         }
-    
-        return PathfindingState.#instance;
+
+        return PathfindingState._instance;
     }
 
     get startNode() {
         return this.graph.startNode;
     }
 
-    /**
-     * 
-     * @param {Number} id OSM node id
-     * @returns {import("./Node").default} node
-     */
     getNode(id) {
         return this.graph?.getNode(id);
     }
 
-    /**
-     * Resets to default state
-     */
     reset() {
         this.finished = false;
-        if(!this.graph) return;
-        for(const key of this.graph.nodes.keys()) {
+        if (!this.graph) return;
+        for (const key of this.graph.nodes.keys()) {
             this.graph.nodes.get(key).reset();
         }
     }
 
-    /**
-     * Resets state and initializes new pathfinding animation
-     */
     start(algorithm) {
         this.reset();
-        switch(algorithm) {
-            case "astar":
-                this.algorithm = new AStar();
-                break;
-            case "greedy":
-                this.algorithm = new Greedy();
-                break;
-            case "dijkstra":
-                this.algorithm = new Dijkstra();
-                break;
-            case "bidirectional":
-                this.algorithm = new BidirectionalSearch();
-                break;
-            default:
-                this.algorithm = new AStar();
-                break;
+        this._nodesExplored = 0;
+
+        const algoNames = {
+            astar: "A*",
+            greedy: "Greedy",
+            dijkstra: "Dijkstra",
+            bidirectional: "Bidirectional",
+        };
+        this.metrics = { ...this.metrics, algorithmName: algoNames[algorithm] ?? "A*" };
+
+        switch (algorithm) {
+            case "astar":         this.algorithm = new AStar(); break;
+            case "greedy":        this.algorithm = new Greedy(); break;
+            case "dijkstra":      this.algorithm = new Dijkstra(); break;
+            case "bidirectional": this.algorithm = new BidirectionalSearch(); break;
+            default:              this.algorithm = new AStar(); break;
         }
 
         this.algorithm.start(this.startNode, this.endNode);
+        this._startTime = performance.now();
     }
 
-    /**
-     * Progresses the pathfinding algorithm by one step/iteration
-     * @returns {(import("./Node").default)[]} array of nodes that were updated
-     */
     nextStep() {
         const updatedNodes = this.algorithm.nextStep();
-        if(this.algorithm.finished || updatedNodes.length === 0) {
+        this._nodesExplored += updatedNodes.length;
+
+        if (this.algorithm.finished || updatedNodes.length === 0) {
             this.finished = true;
+            const execTime = performance.now() - this._startTime;
+
+            let pathLength = 0;
+            let node = this.endNode;
+            while (node?.parent) {
+                pathLength += Math.hypot(
+                    node.longitude - node.parent.longitude,
+                    node.latitude - node.parent.latitude
+                ) * 111;
+                node = node.parent;
+            }
+
+            this.metrics = {
+                ...this.metrics,
+                execTime: execTime.toFixed(1),
+                nodesExplored: this._nodesExplored,
+                pathLength: pathLength.toFixed(2),
+                memoryUsage: this.algorithm.openList?.length ?? 0,
+            };
         }
 
         return updatedNodes;
